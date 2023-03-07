607fa5746350228f381a87029ba940006b849912
Witcher239 <127261146+Witcher239@users.noreply.github.com>
2023-03-08 01:56:04 +0200

    Create Manager.js

---
+++ managers/Manager.js
@@ -0,0 +1,64 @@
+/**@param {NS} ns**/
+
+export class Manager
+{
+	ns;
+
+	waitTime = 60000;
+
+	constructor(ns)
+	{
+		this.ns = ns;
+	}
+
+	async startOperation()
+	{
+		if (!this.validate())
+		{
+			return;
+		}
+
+		this.disableLogs();
+
+		this.init();
+
+		while (true)
+		{
+			await this.manage();
+
+			if (this.shouldStopOperation())
+			{
+				break;
+			}
+			else if (this.waitTime > 0)
+			{
+				await this.ns.sleep(this.waitTime);
+			}
+		}
+	}
+
+	validate()
+	{
+		return true;
+	}
+
+	disableLogs()
+	{
+
+	}
+
+	init()
+	{
+		
+	}
+
+	async manage()
+	{
+		
+	}
+
+	shouldStopOperation()
+	{
+		return false;
+	}
+}
