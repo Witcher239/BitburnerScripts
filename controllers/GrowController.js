d647aef5da18882476b7069484ecda4a4b518290
Witcher239 <127261146+Witcher239@users.noreply.github.com>
2023-03-08 01:48:04 +0200

    Create GrowController.js

---
+++ controllers/GrowController.js
@@ -0,0 +1,61 @@
+/** @param {NS} ns */
+
+import {HackingOperationController} from "blob:file:///8f9040a0-fe36-4c28-8ce6-f083f0ceb807"
+
+export class GrowController extends HackingOperationController
+{
+	constructor(
+		ns,
+		targetServerName)
+	{
+		super(
+			ns,
+			targetServerName);
+
+		this.scriptName = '/scripts/export/grow.js';
+
+		this.scriptRAM = ns.getScriptRam(
+			this.scriptName,
+			'home');	
+	}
+
+	getNumOfThreadsCoefficient(serverName)
+	{
+		var numOfThreadsCoefficient;
+
+		if (serverName == 'home')
+		{
+			numOfThreadsCoefficient = this.getNumOfThreads(this.ns.getServer('home').cpuCores) / this.getNumOfThreads(1);
+		}
+		else
+		{
+			numOfThreadsCoefficient = super.getNumOfThreadsCoefficient(serverName);
+		}
+
+		return numOfThreadsCoefficient;
+	}
+
+	getNumOfThreads(numOfCores = 1)
+	{
+		var maxMoney = this.ns.getServerMaxMoney(this.targetServerName);
+		var currentMoney = this.ns.getServerMoneyAvailable(this.targetServerName);
+
+		if (currentMoney == 0)
+		{
+			currentMoney = 1;
+		}
+
+		var growthMultiplier = maxMoney / currentMoney;
+
+		return Math.ceil(
+			this.ns.growthAnalyze(
+				this.targetServerName,
+				growthMultiplier,
+				numOfCores));
+	}
+
+	getSleepTime()
+	{
+		return this.ns.getGrowTime(this.targetServerName) * 1.1;
+	}
+}
