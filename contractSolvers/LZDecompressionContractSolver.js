51c8619e11560fd214ee50194b46b3e51910777a
Witcher239 <127261146+Witcher239@users.noreply.github.com>
2023-03-08 01:51:21 +0200

    Create LZDecompressionContractSolver.js

---
+++ contractSolvers/LZDecompressionContractSolver.js
@@ -0,0 +1,87 @@
+/** @param {NS} ns */
+
+export class LZDecompressionContractSolver
+{
+	inputExpression = '';
+
+	outputExpression = '';
+
+	constructor(inputExpression)
+	{
+		this.inputExpression = inputExpression;
+	}
+
+	solve()
+	{
+		this.recursiveBuildOutputExpression();
+
+		return this.outputExpression;
+	}
+
+	recursiveBuildOutputExpression(
+		inputExpression = this.inputExpression,
+		isDirectChunk = true)
+	{
+		if (inputExpression != '')
+		{
+			var chunkSize = 1;
+
+			var length = parseInt(
+				inputExpression.substr(
+					0,
+					1));
+
+			if (length != 0)
+			{
+				if (isDirectChunk)
+				{
+					this.decodeDirectChunk(
+						inputExpression,
+						length);
+
+					chunkSize += length;
+				}
+				else
+				{
+					this.decodeReferenceChunk(
+						inputExpression,
+						length);
+
+					chunkSize++;
+				}
+			}
+
+			this.recursiveBuildOutputExpression(
+				inputExpression.substr(
+					chunkSize,
+					inputExpression.length - chunkSize),
+				!isDirectChunk);
+		}
+	}
+
+	decodeDirectChunk(
+		inputExpression,
+		length)
+	{
+		this.outputExpression += inputExpression.substr(
+			1,
+			length);
+	}
+
+	decodeReferenceChunk(
+		inputExpression,
+		length)
+	{
+		var numOfPlacesBefore = parseInt(
+			inputExpression.substr(
+				1,
+				1));
+
+		for (var i = 0; i < length; i++)
+		{
+			this.outputExpression += this.outputExpression.substr(
+				this.outputExpression.length - numOfPlacesBefore,
+				1);
+		}
+	}
+}
