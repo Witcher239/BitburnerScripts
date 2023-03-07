fa07a411ce88ef8f8178447cae1a170b36e7a5ee
Witcher239 <127261146+Witcher239@users.noreply.github.com>
2023-03-08 01:44:18 +0200

    Create util.js

---
+++ util/util.js
@@ -0,0 +1,134 @@
+/** @param {NS} ns */
+
+export function getAllServerNames(
+	ns,
+	serverName = 'home',
+	allServerNames = ['home'])
+{
+	var connectedServerNames = ns.scan(serverName);
+
+	for (var i = 0; i < connectedServerNames.length; i++)
+	{
+		var connectedServerName = connectedServerNames[i];
+
+		if (allServerNames.indexOf(connectedServerName) == -1)
+		{
+			allServerNames.push(connectedServerName);
+
+			allServerNames = getAllServerNames(
+				ns,
+				connectedServerName,
+				allServerNames);
+		}
+	}
+
+	return allServerNames;
+}
+
+export function getPurchasedServerNames(ns)
+{
+	var purchasedServerNames = [];
+
+	var connectedServerNames = ns.scan('home');
+
+	for (var i = 0; i < connectedServerNames.length; i++)
+	{
+		var connectedServerName = connectedServerNames[i];
+
+		if (connectedServerName.search('hack') != -1)
+		{
+			purchasedServerNames.push(connectedServerName);
+		}
+	}
+
+	return purchasedServerNames;
+}
+
+export async function executeOnRemoteServer(
+	ns,
+	scriptName,
+	serverName,
+	numThreads = 1,
+	args = [])
+{
+	var successfullRun = false;
+
+	var serverFreeRAM = getServerFreeRAM(
+		ns,
+		serverName);
+
+	var scriptRAM = ns.getScriptRam(
+		scriptName,
+		ns.getHostname());
+
+	if (numThreads == 'max')
+	{
+		numThreads = scriptRAM == 0 ?
+			1 :
+			Math.floor(serverFreeRAM / scriptRAM);
+
+		if (numThreads == 0)
+		{
+			numThreads = 1;
+		}
+	}
+
+	var requiredRAM = scriptRAM * numThreads;
+
+	if (serverFreeRAM >= requiredRAM)
+	{
+		await ns.scp(
+			scriptName,
+			serverName);
+
+		if (await ns.exec(
+				scriptName,
+				serverName,
+				numThreads,
+				...args) != 0)
+		{
+			successfullRun = true;
+		}
+	}
+	else
+	{
+		ns.print(
+			'There is not enough RAM available on the server '
+			+ serverName
+			+ ' to run a script '
+			+ scriptName
+			+ ' with a number of threads '
+			+ numThreads
+			+ '. RAM required: '
+			+ ns.formatRam(requiredRAM)
+			+ ' ('
+			+ ns.formatRam(scriptRAM)
+			+ ' per thread). Available RAM: '
+			+ ns.formatRam(serverFreeRAM)
+			+ ' (Max: '
+			+ ns.formatRam(ns.getServerMaxRam(serverName))
+			+ ')');
+	}
+
+	return successfullRun;	
+}
+
+export function getServerFreeRAM(
+	ns,
+	serverName)
+{
+	var maxRAM = ns.getServerMaxRam(serverName);
+
+	var currentScriptRAM = 0;
+
+	if (serverName == ns.getHostname())
+	{
+		currentScriptRAM = ns.getScriptRam(
+			ns.getScriptName(),
+			serverName);
+	}
+
+	var freeRAM = maxRAM - ns.getServerUsedRam(serverName) + currentScriptRAM;
+	
+	return freeRAM;
+}
