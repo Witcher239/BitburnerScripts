42e30fcea2d2b0da93f2443f12ab6bf81a16bb1a
Witcher239 <127261146+Witcher239@users.noreply.github.com>
2023-03-08 01:52:06 +0200

    Create PrimeFactorContractSolver.js

---
+++ contractSolvers/PrimeFactorContractSolver.js
@@ -0,0 +1,52 @@
+/** @param {NS} ns */
+
+export class PrimeFactorContractSolver
+{
+	inputNumber = 0;
+
+	primeFactors = [];
+
+	constructor(inputNumber)
+	{
+		this.inputNumber = inputNumber;
+	}
+
+	solve()
+	{
+		this.doRecursivePrimeFactorsSearch(this.inputNumber);
+
+		return this.largestPrimeFactor();
+	}
+
+	doRecursivePrimeFactorsSearch(number)
+	{
+		for (var i = 2; i <= number; i++)
+		{
+			if (number % i == 0)
+			{
+				this.primeFactors.push(i);
+
+				this.doRecursivePrimeFactorsSearch(number / i);
+
+				break;
+			}
+		}
+	}
+
+	largestPrimeFactor()
+	{
+		var largestPrimeFactor = 0;
+
+		for (var i = 0; i < this.primeFactors.length; i++)
+		{
+			var primeFactor = this.primeFactors[i];
+
+			if (primeFactor > largestPrimeFactor)
+			{
+				largestPrimeFactor = primeFactor;
+			}
+		}
+
+		return largestPrimeFactor;
+	}
+}
