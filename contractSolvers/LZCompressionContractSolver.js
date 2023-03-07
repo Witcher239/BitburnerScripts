d251d8c6aacc7a4f8b1cb2494eaa7248fafe553f
Witcher239 <127261146+Witcher239@users.noreply.github.com>
2023-03-08 01:50:55 +0200

    Create LZCompressionContractSolver.js

---
+++ contractSolvers/LZCompressionContractSolver.js
@@ -0,0 +1,194 @@
+/** @param {NS} ns */
+
+export class LZCompressionContractSolver
+{
+	inputExpression = '';
+
+	outputExpression = '';
+
+	constructor(inputExpression)
+	{
+		this.inputExpression = inputExpression;
+	}
+
+	solve()
+	{
+		this.recursiveEncodeDirect();
+
+		return this.outputExpression;
+	}
+
+	recursiveEncodeDirect(
+		startingIndex = 0,
+		outputExpression = '',
+		canSkipChunk = false)
+	{
+		if (!this.tryToSaveEncoding(
+				startingIndex,
+				outputExpression))
+		{
+			var bestEncodingLengthFound = 0;
+
+			var bestEncodedLengthFound = 0;
+
+			var bestOutputExpression = '';
+
+			var canSkipNextChunk = true;
+
+			var minChunkLength = canSkipChunk ?
+				0 :
+				1;
+
+			var maxChunkLength = Math.min(
+				9,
+				this.inputExpression.length - startingIndex);
+
+			for (var i = minChunkLength; i <= maxChunkLength; i++)
+			{
+				var directOutputExpression = '' + i;
+
+				var textToEncode = this.inputExpression.substr(
+					startingIndex,
+					i);
+
+				directOutputExpression += textToEncode;
+
+				var encodingLength = directOutputExpression.length;
+				var encodedLength = i;
+				var newOutputExpression = directOutputExpression;
+
+				if (this.tryToSaveEncoding(
+						startingIndex + i,
+						outputExpression + newOutputExpression))
+				{
+					if (bestEncodingLengthFound == 0
+						|| (bestEncodedLengthFound / bestEncodingLengthFound) < (encodedLength / encodingLength))
+					{
+						bestEncodingLengthFound = encodingLength;
+
+						bestEncodedLengthFound = encodedLength;
+
+						bestOutputExpression = newOutputExpression;
+					}
+
+					break;
+				}
+				else
+				{
+					var referenceLength = 0;
+					var referenceOutputExpression = '';
+
+					[referenceLength, referenceOutputExpression] = this.encodeReference(
+						startingIndex + i,
+						i != 0);
+
+					encodingLength += referenceOutputExpression.length;
+					encodedLength += referenceLength;
+					newOutputExpression += referenceOutputExpression;
+
+					if (i + referenceLength > 0
+						&& (bestEncodingLengthFound == 0
+						 || (bestEncodedLengthFound / bestEncodingLengthFound) < (encodedLength / encodingLength)))
+					{
+						bestEncodingLengthFound = encodingLength;
+
+						bestEncodedLengthFound = encodedLength;
+
+						bestOutputExpression = newOutputExpression;
+
+						canSkipNextChunk = referenceLength != 0;
+					}
+				}
+			}
+
+			this.recursiveEncodeDirect(
+				startingIndex + bestEncodedLengthFound,
+				outputExpression + bestOutputExpression,
+				canSkipNextChunk);
+		}
+	}
+
+	tryToSaveEncoding(
+		startingIndex,
+		outputExpression)
+	{
+		var ret = false;
+
+		if (startingIndex == this.inputExpression.length
+			&& outputExpression != '')
+		{
+			if (this.outputExpression.length == 0
+				|| this.outputExpression.length > outputExpression.length)
+			{
+				this.outputExpression = outputExpression;
+			}
+
+			ret = true;
+		}
+
+		return ret;
+	}
+
+	encodeReference(
+		startingIndex = 0,
+		canSkipChunk = false)
+	{
+		var bestLengthFound = 0;
+		var referenceExpression = '';
+
+		var minChunkLength = canSkipChunk ?
+			0 :
+			1;
+
+		var maxChunkLength = Math.min(
+			9,
+			this.inputExpression.length - startingIndex);
+
+		for (var i = maxChunkLength; i >= minChunkLength; i--)
+		{
+			referenceExpression = '' + i;
+
+			if (i == 0)
+			{
+				bestLengthFound = i;
+			}
+			else
+			{
+				var positionsBackFound = 0;
+
+				var textToEncode = this.inputExpression.substr(
+					startingIndex,
+					i);
+
+				var maxPositionsBack = Math.min(
+					9,
+					startingIndex);
+
+				for (var j = 1; j <= maxPositionsBack; j++)
+				{
+					var referenceText = this.inputExpression.substr(
+						startingIndex - j,
+						i);
+
+					if (referenceText == textToEncode)
+					{
+						positionsBackFound = j;
+
+						break;
+					}
+				}
+
+				if (positionsBackFound > 0)
+				{
+					bestLengthFound = i;
+
+					referenceExpression += positionsBackFound;
+
+					break;
+				}		
+			}
+		}
+
+		return [bestLengthFound, referenceExpression]
+	}
+}
