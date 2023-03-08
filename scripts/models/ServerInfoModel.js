a85754b9200daebf1cb4680f91da69517f2c4bf2
Witcher239 <127261146+Witcher239@users.noreply.github.com>
2023-03-08 02:20:46 +0200

    Create ServerInfoModel.js

---
+++ scripts/models/ServerInfoModel.js
@@ -0,0 +1,15 @@
+/** @param {NS} ns */
+
+export class ServerInfoModel
+{
+	name = '';
+
+	securityLevel = 0;
+	minSecurityLevel = 0;
+
+	moneyAvailable = 0;
+	maxMoney = 0;
+
+	freeRAM = 0;
+	maxRAM = 0;
+}
