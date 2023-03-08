80fd146b79d83dc8b3c9b1fd6167f824f945d2ed
Witcher239 <127261146+Witcher239@users.noreply.github.com>
2023-03-08 02:19:29 +0200

    Create searchContracts.js

---
+++ scripts/searchContracts.js
@@ -0,0 +1,33 @@
+/** @param {NS} ns */
+
+import {getAllServerNames} from "/scripts/util/util.js"
+
+export async function main(ns)
+{
+	var serverNames = getAllServerNames(ns).sort();
+
+	for (var i = 0; i < serverNames.length; i++)
+	{
+		var serverName = serverNames[i];
+
+		var contractNames = ns.ls(
+			serverName,
+			".cct");
+
+		for (var j = 0; j < contractNames.length; j++)
+		{
+			var contractName = contractNames[j];
+
+			ns.tprint(
+				"Server '"
+				+ serverName
+				+ "' contains contract '"
+				+ contractName
+				+ "' of type '"
+				+ ns.codingcontract.getContractType(
+					contractName,
+					serverName)
+				+ "'");
+		}
+	}
+}
