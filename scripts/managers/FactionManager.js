e9bc4bceb2e0039345c64dafc81d9e48e4a05b06
Witcher239 <127261146+Witcher239@users.noreply.github.com>
2023-10-21 19:02:43 +0300

    Create FactionManager.js

---
+++ scripts/managers/FactionManager.js
@@ -0,0 +1,35 @@
+/**@param {NS} ns**/
+
+import {Manager} from "/scripts/managers/Manager.js"
+
+export async function main(ns)
+{
+	var factionManager = new FactionManager(ns);
+
+	await factionManager.startOperation();
+}
+
+export class FactionManager extends Manager
+{
+	constructor(ns)
+	{
+		super(ns);
+	}
+
+	async manage()
+	{
+		this.processInvitations();
+	}
+
+	processInvitations()
+	{
+		var factionInvitations = this.ns.singularity.checkFactionInvitations();
+
+		for (var i = 0; i < factionInvitations.length; i++)
+		{
+			var factionName = factionInvitations[i];
+
+			this.ns.singularity.joinFaction(factionName);
+		}
+	}
+}
