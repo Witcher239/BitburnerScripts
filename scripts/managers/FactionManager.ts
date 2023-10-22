b5011915399caf8e7e8943203319fb67c6ab9c0d
Borys Dykukha <Borys.Dykukha@ontargit.com>
2023-10-22 12:59:18 +0300

    Added city faction group checks for automatical join of city factions

--- scripts/managers/FactionManager.ts
+++ scripts/managers/FactionManager.ts
@@ -29,7 +29,142 @@ export class FactionManager extends Manager
 		{
 			var factionName = factionInvitations[i];
 
-			this.ns.singularity.joinFaction(factionName);
+			this.processInvitation(factionName);
 		}
 	}
+
+	processInvitation(factionName: string)
+	{
+		if (this.shouldJoinFaction(factionName))
+		{
+			this.ns.singularity.joinFaction(factionName);
+        }
+	}
+
+	shouldJoinFaction(factionName: string)
+	{
+		var ret = true;
+
+		var cityFactionGroup = this.getCityFactionGroup(factionName);
+
+		if (cityFactionGroup != '')
+		{
+			ret =
+				this.belongToCityFactionGroup(cityFactionGroup)
+				|| this.augmentationsLeftInCityFactionGroup(cityFactionGroup);
+        }
+
+		return ret;
+    }
+
+	getCityFactionGroup(factionName: string)
+	{
+		var cityFactionGroup = '';
+
+		switch (factionName)
+		{
+			case this.ns.enums.CityName.Sector12:
+			case this.ns.enums.CityName.Aevum:
+
+				cityFactionGroup = 'America';
+
+				break;
+
+			case this.ns.enums.CityName.Chongqing:
+			case this.ns.enums.CityName.NewTokyo:
+			case this.ns.enums.CityName.Ishima:
+
+				cityFactionGroup = 'Asia';
+
+				break;
+
+			case this.ns.enums.CityName.Volhaven:
+
+				cityFactionGroup = 'Europe';
+
+				break;
+		}
+
+		return cityFactionGroup;
+	}
+
+	belongToCityFactionGroup(cityFactionGroup: string)
+	{
+		var ret = false;
+
+		switch (cityFactionGroup)
+		{
+			case 'America':
+
+				ret =
+					this.ns.getPlayer().factions.includes(this.ns.enums.CityName.Sector12)
+					|| this.ns.getPlayer().factions.includes(this.ns.enums.CityName.Aevum);
+
+				break;
+
+			case 'Asia':
+
+				ret =
+					this.ns.getPlayer().factions.includes(this.ns.enums.CityName.Chongqing)
+					|| this.ns.getPlayer().factions.includes(this.ns.enums.CityName.NewTokyo)
+					|| this.ns.getPlayer().factions.includes(this.ns.enums.CityName.Ishima);
+
+				break;
+
+			case 'Europe':
+
+				ret = this.ns.getPlayer().factions.includes(this.ns.enums.CityName.Volhaven);
+
+				break;
+        }
+
+		return ret;
+	}
+
+	augmentationsLeftInCityFactionGroup(cityFactionGroup: string)
+	{
+		var ret = false;
+
+		var purchasedAugmentations = this.ns.singularity.getOwnedAugmentations(true);
+
+		var cityFactionGroupAugmentations: string[] = [];
+
+		switch (cityFactionGroup)
+		{
+			case 'America':
+
+				cityFactionGroupAugmentations.push(...this.ns.singularity.getAugmentationsFromFaction(this.ns.enums.CityName.Sector12));
+				cityFactionGroupAugmentations.push(...this.ns.singularity.getAugmentationsFromFaction(this.ns.enums.CityName.Aevum));
+
+				break;
+
+			case 'Asia':
+
+				cityFactionGroupAugmentations.push(...this.ns.singularity.getAugmentationsFromFaction(this.ns.enums.CityName.Chongqing));
+				cityFactionGroupAugmentations.push(...this.ns.singularity.getAugmentationsFromFaction(this.ns.enums.CityName.NewTokyo));
+				cityFactionGroupAugmentations.push(...this.ns.singularity.getAugmentationsFromFaction(this.ns.enums.CityName.Ishima));
+
+				break;
+
+			case 'Europe':
+
+				cityFactionGroupAugmentations.push(...this.ns.singularity.getAugmentationsFromFaction(this.ns.enums.CityName.Volhaven));
+
+				break;
+		}
+
+		for (var i = 0; i < cityFactionGroupAugmentations.length; i++)
+		{
+			var augmentationName = cityFactionGroupAugmentations[i];
+
+			if (!purchasedAugmentations.includes(augmentationName))
+			{
+				ret = true;
+
+				break;
+            }
+        }
+
+		return ret;
+    }
 }
